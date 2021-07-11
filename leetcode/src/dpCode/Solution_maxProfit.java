06f6993e9ca5e0d55a303ebac7cfd825a9b2cefc
xingzz <xingzz97@163.com>
2021-07-11 16:00:59 +0800

    股票买卖集合

--- leetcode/src/dpCode/Solution_maxProfit.java
+++ leetcode/src/dpCode/Solution_maxProfit.java
@@ -2,29 +2,60 @@ package dpCode;
 
 public class Solution_maxProfit {
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{7,6,4,3,1}));
+        System.out.println(maxProfit(new int[]{1, 3, 2, 8, 4, 9},2));
     }
-    //多次买卖
-    public static int maxProfit(int[] prices) {
-        int len = prices.length;
-        int temp = 0;
-        int sum = 0;
-        int min = prices[0];
-        if (len >= 2){
-            for (int i = 1; i < len; i++) {
-                if(prices[i] < prices[i - 1]){
-                    min = prices[i];
-                    sum += temp;
-                    temp = 0;
-                }else {
-                    temp = Math.max(temp,prices[i] - min);
-                    min = Math.min(min,prices[i]);
-                }
-            }
-            sum += temp;
+    //多次买卖，包含手续费
+    public static int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int sell = 0, buy = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            sell = Math.max(sell, buy + prices[i] - fee);
+            buy = Math.max(buy, sell - prices[i]);
         }
-        return sum;
+        return sell;
     }
+
+    //多次买卖，包含冷冻期
+//    public static int maxProfit(int[] prices) {
+//        if (prices.length == 0) {
+//            return 0;
+//        }
+//
+//        int n = prices.length;
+//        // f[i][0]: 手上持有股票的最大收益
+//        // f[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
+//        // f[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
+//        int[][] f = new int[n][3];
+//        f[0][0] = -prices[0];
+//        for (int i = 1; i < n; ++i) {
+//            f[i][0] = Math.max(f[i - 1][0], f[i - 1][2] - prices[i]);
+//            f[i][1] = f[i - 1][0] + prices[i];
+//            f[i][2] = Math.max(f[i - 1][1], f[i - 1][2]);
+//        }
+//        return Math.max(f[n - 1][1], f[n - 1][2]);
+//    }
+
+    //多次买卖不含冷冻期
+//    public static int maxProfit(int[] prices) {
+//        int len = prices.length;
+//        int temp = 0;
+//        int sum = 0;
+//        int min = prices[0];
+//        if (len >= 2){
+//            for (int i = 1; i < len; i++) {
+//                if(prices[i] < prices[i - 1]){
+//                    min = prices[i];
+//                    sum += temp;
+//                    temp = 0;
+//                }else {
+//                    temp = Math.max(temp,prices[i] - min);
+//                    min = Math.min(min,prices[i]);
+//                }
+//            }
+//            sum += temp;
+//        }
+//        return sum;
+//    }
 
     //单次买卖
 //    public static int maxProfit(int[] prices) {
