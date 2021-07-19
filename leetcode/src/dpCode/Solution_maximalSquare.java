28bb433b47bb8969e96c23dbf070fc73c6733481
Damonforever <xingzz97@163.com>
2021-07-19 23:46:19 +0800

    最大正方形修正

--- leetcode/src/dpCode/Solution_maximalSquare.java
+++ leetcode/src/dpCode/Solution_maximalSquare.java
@@ -12,19 +12,25 @@ public class Solution_maximalSquare {
         int[][] dp = new int[m][n];
         int max = 0;
         for (int i = 0; i < n; i++) {
-            if (matrix[0][i] == 1) dp[0][i] = 1;
+            if (matrix[0][i] == '1') {
+                dp[0][i] = 1;
+                max = 1;
+            }
         }
         for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 1) dp[i][0] = 1;
+            if (matrix[i][0] == '1') {
+                dp[i][0] = 1;
+                max = 1;
+            }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) continue;
-                else {
-
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j-1],dp[i-1][j]),dp[i][j-1])+1;
+                    max = Math.max(max,dp[i][j]);
                 }
             }
         }
-        return max;
+        return max*max;
     }
 }
